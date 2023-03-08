f2c57f4870a7070db57e0a8df6a64916b8bb6864
Navam <navamsharma142@gmail.com>
2023-03-08 13:34:28 +0530

    First Commit

---
+++ ascii.c
@@ -0,0 +1,10 @@
+// This code prints the ascii value corresponding to that number
+#include<stdio.h>
+int main() {
+    int i = 0;
+    while(i <= 256) {
+        printf("%d: %c\n",i,i);
+        i += 1;
+    }
+    return 0;
+}
